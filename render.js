2ca47af8121e682fa9179039b3c5a9de80e83214
Louis Kruger <louisk@gmail.com>
2019-09-05 17:37:36 -0400

    add duktape integrations

---
+++ render.js
@@ -0,0 +1,6 @@
+// function draw_rectangle(x, y, w, h, r, g, b);
+
+draw_rectangle(300, 100, 200, 100, 0xb0, 0x20, 0x20);
+draw_rectangle(900, 100, 100, 200, 0x20, 0xb0, 0x20);
+draw_rectangle(400, 400, 400, 150, 0x20, 0x20, 0xb0);
+
